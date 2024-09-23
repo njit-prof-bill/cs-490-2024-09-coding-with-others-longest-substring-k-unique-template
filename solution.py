3d6bc2687a2cf07cc517039411519a757b75bcdb
Charles Wang <quagum125@gmail.com>
2024-09-23 19:33:09 -0400

    Revert "Implemented solution for longest substring with K unique characters"
    This reverts commit cbb58262a7895030684bf52781d9415cacb9bc6c.

--- solution.py
+++ solution.py
@@ -1,25 +1,3 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
     # Write your code here
-    answer = 0
-    left = 0
-    right = left + 1
-    unique_chars = {}
-
-    while right < len(s):
-
-        if unique_chars.get(s[right], 0) == 0:
-            unique_chars[s[right]] = 1
-            
-        while len(unique_chars.keys()) > k:
-            del unique_chars[s[left]]
-            left += 1
-        
-        answer = max(right-left+1, answer)
-        right += 1
-        
-    return answer
-
-s = "cbbebi"
-k = 3
-
-print(longest_substring_k_unique(s, k))
+    pass
