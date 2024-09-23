60998f5eea80b2727d1cdf4627ec678e3f551d65
Ryan <squekkyy@gmail.com>
2024-09-23 19:57:18 -0400

    Implemented longest string function

--- solution.py
+++ solution.py
@@ -1,3 +1,20 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
     # Write your code here
-    pass
+    longest = [""]
+    for start in range(len(s)+1):
+        for end in range(start+1,len(s)+1):
+            substr = s[start:end]
+            substrDict = {i: substr.count(i) for i in set(substr)}
+            if (len(substrDict) == k):
+                if (len(substr) > len(longest[0])):
+                    longest = [substr]
+                elif (len(substr) == len(longest[0])):
+                    longest.append(substr)
+    return longest
+
+tests = [("araaci", 2), ("cbbebi", 3), ("aa", 1)]
+for test in tests:
+    longestSubstring = longest_substring_k_unique(test[0], test[1])
+    print(f"\nInput: S = {test[0]}, K = {test[1]}\n"
+        f"Output: {len(longestSubstring[0])}\n"
+        f"Longest Substrings: {', '.join(longestSubstring)}")
