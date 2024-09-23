606f3c2ae8d8c43112652fee581a2c1b239c83ca
kb5466 <113871858+kb5466@users.noreply.github.com>
2024-09-23 19:37:40 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,13 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
-    # Write your code here
-    pass
+    retCount = 0
+    charCount = 0
+    arr = []
+    for char in s:
+        if char not in arr:
+            if charCount !=k:
+                retCount = retCount + 1
+                charCount = charCount + 1
+                arr.append(char)
+        else:
+            retCount = retCount + 1
+    return retCount
