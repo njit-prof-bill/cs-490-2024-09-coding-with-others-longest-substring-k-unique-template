9bf1e3a0fb30dcda69f8234b9dc135190870b76e
Alex Ack <ackalex16@gmail.com>
2024-09-23 20:07:17 -0400

    completed the longest substring function

--- solution.py
+++ solution.py
@@ -1,3 +1,8 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
-    # Write your code here
+    maxlen = -1
+    for start in range(len(s)):
+        for end in range(start + 1, len(s) + 1):
+            if (len(set(s[start:end])) == k):
+                maxlen = max(maxlen, end-start)
+    return(maxlen)
     pass
