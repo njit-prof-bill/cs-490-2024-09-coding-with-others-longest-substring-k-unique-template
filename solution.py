e4e8207e528af13ccf7c90a6fc9cd6f1d7506b14
Jhay Cudia <jc383@njit.edu>
2024-09-24 02:36:14 -0400

    added extra comments for clarity

--- solution.py
+++ solution.py
@@ -23,6 +23,17 @@ def longest_substring_k_unique(s: str, k: int) -> int:
         # update the char's frequency
         char_freq[char_idx] += 1
 
+        '''
+        If the number of unique characters becomes greater than k, then our goal
+        is to remove one of the unique characters from the set until the number of
+        unique characters is equal to k again. This is done by shrinking the window
+        from the left with the left pointer 'start'. But if we do this, we need to 
+        ensure that the character that we just removed from the window doesn't have 
+        duplicates currently still in the newly, shrunken window. If that is the case, 
+        then we cannot remove the character from the set. This is why we are storing 
+        the frequencies of each character in 'char_freq'. Once a character's frequency 
+        reaches 0, only then can we remove it from the set.
+        '''
         while len(unique) > k:
             char_idx = ord(s[start]) - ord('a') 
             char_freq[char_idx] -= 1
@@ -31,6 +42,10 @@ def longest_substring_k_unique(s: str, k: int) -> int:
                 unique.remove(s[start])
             start += 1
         
+        '''
+        Only update 'max_len' when the number of unique characters equals to k.
+        Otherwise, if that never happens, return max_len's initial value of 0.
+        '''
         if len(unique) == k:
             curr_len = end - start + 1
             max_len = max(curr_len, max_len)
