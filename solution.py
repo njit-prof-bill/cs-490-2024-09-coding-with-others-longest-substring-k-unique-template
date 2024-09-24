4f8a38eae643ec00d8f680766db03345ff43e022
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2024-09-23 20:04:05 -0400

    Solution

--- solution.py
+++ solution.py
@@ -1,3 +1,31 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
     # Write your code here
+    variations = []
+    curr_letter = s[0]
+    changes = 0
+    curr_str = ""
+    for letter in s:
+        if letter == curr_letter:
+            curr_str += letter
+            continue
+        elif changes == k or len(curr_str) == len(s):
+            variations.append(curr_str)
+            changes = 0
+            curr_str = ""
+            break
+        else:
+            curr_str += letter
+            curr_letter = letter
+            changes += 1
+            continue
+    if curr_str not in variations and curr_str != "":
+        variations.append(curr_str)
+
+    sorted_var = sorted(variations, key=lambda x: len(x))
+    print(sorted_var[-1])
     pass
+
+
+longest_substring_k_unique("araaci", 2)
+longest_substring_k_unique("cbbebi", 3)
+longest_substring_k_unique("aa", 1)
