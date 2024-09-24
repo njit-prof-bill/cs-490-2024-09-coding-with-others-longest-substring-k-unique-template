531e5082b868051b9c8c4bd1ef0b7571f0a58460
da526 <da526@njit.edu>
2024-09-23 20:05:36 -0400

    Implemented solution for longest substring with K unique characters

--- solution.py
+++ solution.py
@@ -1,3 +1,21 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
-    # Write your code here
-    pass
+    n = len(s)
+    cnt = [0] * 26
+    i = j = m = res = 0
+    while j < n:
+        if m <= k:
+            idx = ord(s[j])-ord('a')
+            if cnt[idx] == 0:
+                m += 1
+            cnt[idx] += 1
+            j += 1
+            if m == k:
+                res = max(res, j - i)
+        else:
+            idx = ord(s[i])-ord('a')
+            cnt[idx] -= 1
+            if cnt[idx] == 0:
+                m -= 1
+            i += 1
+    return res
+    
