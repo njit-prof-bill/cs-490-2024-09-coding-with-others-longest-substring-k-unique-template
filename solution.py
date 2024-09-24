4faf72fe6ccfaaaf08f150d8ad2dee18333e11a7
Rocko Tompkins <rock4259@hotmail.com>
2024-09-23 22:31:18 -0400

    fixed when number of unique characters in string does not exceed k

--- solution.py
+++ solution.py
@@ -1,17 +1,14 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
     
     letter = []
+    maxLen = 0
+    currentIndex = 0
+    endIndex = 0
 
     if k > len(s) or len(s) == 0:
         return 0
     
-    maxLen = 0
-
-    currentIndex = 0
-    endIndex = 0
-
     for char in s:
-
         bChar = char in letter
         inRangeForAdd = len(letter) != k
 
@@ -23,11 +20,16 @@ def longest_substring_k_unique(s: str, k: int) -> int:
             
         currentIndex += 1
     
-    if endIndex == 0:
-        return currentIndex
+
+    if endIndex == 0:#if end was reached
+        maxLen = currentIndex
+    else:
+        maxLen = endIndex
+
+    if len(letter) < k:#if max len found was less than k
+        maxLen = 0
     
-    maxLen = endIndex
-    
+    #find subsequent patterns
     nextMaxLen = longest_substring_k_unique(s[1:], k)
     
     if (maxLen < nextMaxLen):
@@ -35,8 +37,7 @@ def longest_substring_k_unique(s: str, k: int) -> int:
    
     return maxLen
 
-
-
 print(longest_substring_k_unique("araaci", 2))
 print(longest_substring_k_unique("cbbebi", 3))
 print(longest_substring_k_unique("aa", 1))
+print(longest_substring_k_unique("araa", 3))
