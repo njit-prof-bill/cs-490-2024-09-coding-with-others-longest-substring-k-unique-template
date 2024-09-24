ac5bcfb98c34e8fd9b6072486b8c2f6418ba37d1
RonShan03 <76448621+RonShan03@users.noreply.github.com>
2024-09-23 20:56:30 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,33 @@
-def longest_substring_k_unique(s: str, k: int) -> int:
-    # Write your code here
-    pass
+def longest_substring_k_unique(s, k):
+    n = len(s)
+    if n == 0 or k == 0:
+        return -1
+
+    char_count = {}
+    left = 0
+    answer = -1
+
+    for right in range(n):
+        char_count[s[right]] = char_count.get(s[right], 0) + 1
+        
+        while len(char_count) > k:
+            char_count[s[left]] -= 1
+            if char_count[s[left]] == 0:
+                del char_count[s[left]]
+            left += 1
+        
+        if len(char_count) == k:
+            answer = max(answer, right - left + 1)
+    print(answer)
+    return answer
+
+
+s1 = "araaci"
+s2 = "cbbebi"
+s3 = "aa"
+
+
+# Testing
+longest_substring_k_unique(s1, 3)
+longest_substring_k_unique(s2, 2)
+longest_substring_k_unique(s3, 1)
