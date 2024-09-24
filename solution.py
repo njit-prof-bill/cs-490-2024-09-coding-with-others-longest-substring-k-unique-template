b95edc90245a770800aa04f45891d7fd4751884b
kb5466 <113871858+kb5466@users.noreply.github.com>
2024-09-24 01:35:13 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,24 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
     retCount = 0
-    charCount = 0
-    arr = []
-    for char in s:
-        if char not in arr:
-            if charCount !=k:
-                retCount = retCount + 1
-                charCount = charCount + 1
-                arr.append(char)
-        else:
-            retCount = retCount + 1
+    for i in range(len(s)):
+        charCount = 0
+        sub = ""
+        print("ROUND: ", i)
+        #Check substring starting from i
+        for j in range(i,len(s)):
+            # when charCount is at limit and not in the substring
+            if charCount == k and s[j] not in sub:
+                break
+            
+            # when charCount is not at limit and not in substring
+            elif charCount < k and s[j] not in sub:
+                charCount +=1
+                sub+=s[j]
+            
+            #when charCount maybe at limit but character is already there
+            elif charCount<=k and s[j] in sub :
+                sub += s[j]
+        #if the substring is longer than current return count then we change
+        if len(sub) > retCount:
+            retCount = len(sub)
     return retCount
