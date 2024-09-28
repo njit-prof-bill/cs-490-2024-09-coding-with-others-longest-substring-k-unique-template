3f6a70837506c5f3abe8958b8a4d47a85725d4db
Noah Paul <npaul4@Noahs-MacBook-Air.local>
2024-09-27 20:49:43 -0400

    Added code

--- solution.py
+++ solution.py
@@ -1,3 +1,26 @@
 def longest_substring_k_unique(s: str, k: int) -> int:
     # Write your code here
-    pass
+    counts = {}
+    res = 0
+    uniq_count = 0
+    left = 0
+
+    for right in range(len(s)):
+        rchar = s[right]
+        if rchar not in counts or counts[rchar] == 0:
+            counts[rchar] = 1
+            uniq_count += 1
+        else:
+            counts[rchar] += 1
+
+        while uniq_count > k:
+            lchar = s[left]
+            counts[lchar] -= 1
+            if counts[lchar] == 0:
+                uniq_count -= 1
+            left += 1
+
+        if uniq_count == k:
+            res = max(res, right - left + 1)
+
+    return res
